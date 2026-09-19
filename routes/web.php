0c46f378a5076a03b36b2d46213402760a49d659
satriomaulah-ctrl <satriomaulah@gmail.com>
2026-09-19 17:50:03 +0700

    Update nama, foto, dan perbaikan view mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/mahasiswa', function () {
+    $mahasiswa = [
+        'status' => 'Aktif',
+        'nama' => 'Satrio Maula Hafiz',
+        'nim' => '2510114013',
+        'prodi' => 'sistem informasi',
+        'email' => 'satriohafiz0307@gmail.com',
+        'kampus' => 'Universitas Pamulang',
+    ];
+    return view('mahasiswacontroler', compact('mahasiswa'));
+});
